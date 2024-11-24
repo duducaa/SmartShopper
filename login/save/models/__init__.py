f0447ee764179205148c327dc2fd7c5314a2bf01
duducaa <duduca1881@gmail.com>
2024-11-24 02:14:08 -0300

    reset the repo

--- login/save/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from desired_products import Desired_Products
-from prices import Prices
-from stores import Stores
--- login/save/utils.py
+++
@@ -1,3 +0,0 @@
-def b64image(path):
-    image = open(os.getcwd() + path, 'rb')
-    return base64.b64encode(image.read()).decode("utf-8")
